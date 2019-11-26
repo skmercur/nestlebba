6444154a121225327ddd9ff3f0cf226e95f6e62b
sofiane khoudour <khoudoursofiane75@gmail.com>
2019-11-26 17:54:50 +0100

    Updates

---
+++ precache-manifest.3985161b1a223e4bb604df98176f30e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f7fdeecfc64a31aa63ec69e83175f7",
+    "url": "/nestlebba/index.html"
+  },
+  {
+    "revision": "ac6fe993e86cc5938540",
+    "url": "/nestlebba/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "03a7abbf6825f5c9a867",
+    "url": "/nestlebba/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ac6fe993e86cc5938540",
+    "url": "/nestlebba/static/js/2.7d333ef9.chunk.js"
+  },
+  {
+    "revision": "03a7abbf6825f5c9a867",
+    "url": "/nestlebba/static/js/main.b6852458.chunk.js"
+  },
+  {
+    "revision": "5b8ab2ef4d9800edd82b",
+    "url": "/nestlebba/static/js/runtime-main.7930401e.js"
+  }
+]);
